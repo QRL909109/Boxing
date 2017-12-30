e6ff1c7a4a5c94c802d50e22c0c1bf8372eb1c71
QRL <qiuruilin@kuaizi.co>
2017-12-31 00:21:27 +0800

    增加头条 轮播

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,74 +1,46 @@
 <!-- 默认首页头条 -->
 <template>
-  <div>
-    {{msg}}新闻
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-    <p>1</p>
-  </div>
+  <section>
+      <swiper 
+      :list="swiperList" 
+      auto
+      loop
+      :aspect-ratio="300/800"
+      height="180px"
+      dots-class="custom-bottom" />
+  </section>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        msg: '头条'
-      }
+import { Swiper } from 'vux'
+// 测试图片 后面删除
+import pic1 from '@/assets/img/1.jpeg'
+import pic2 from '@/assets/img/2.png'
+import pic3 from '@/assets/img/3.png'
+import pic4 from '@/assets/img/4.jpeg'
+export default {
+  data () {
+    return {
+      swiperList: [{
+        url: 'javascript:',
+        img: pic1,
+        title: '拳手赛前应该禁欲吗？'
+      }, {
+        url: 'javascript:',
+        img: pic2,
+        title: '专门狙杀世界冠军，KO安德雷击溃叶翔后，这个伊朗血魔欲制霸重量级'
+      }, {
+        url: 'javascript:',
+        img: pic3,
+        title: '美丽死神和穆塞在这算酱油，这家拳馆的历史让世界膜拜！'
+      }, {
+        url: 'javascript:',
+        img: pic4,
+        title: '擂台上“踹脸狂魔”,台下爱看悬疑小说,她是中国最有气势女拳手！'
+      }]
     }
+  },
+  components: {
+    Swiper
   }
+}
 </script>
